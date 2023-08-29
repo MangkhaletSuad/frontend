8dfd32425f12853ec2459f878da10156c4777cfa
MangkhaletSuad <mangkhalet.suad@gmail.com>
2023-08-29 14:27:23 +0700

    Added new feature X

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-import { globIterate } from "glob";
+// import { globIterate } from "gulp";
 
-const gulpImports = [];
+// const gulpImports = [];
 
-for await (const gulpModule of globIterate("build-scripts/gulp/*.?(c|m)js", {
-  dotRelative: true,
-})) {
-  gulpImports.push(import(gulpModule));
-}
+// for await (const gulpModule of globIterate("build-scripts/gulp/*.?(c|m)js", {
+//   dotRelative: true,
+// })) {
+//   gulpImports.push(import(gulpModule));
+// }
 
 // Since all tasks are currently registered with gulp.task(), this is enough
 // If any are converted to named exports, need to loop and aggregate exports here
-await Promise.all(gulpImports);
+// await Promise.all(gulpImports);
+
+const gulp = require('gulp');
+
+gulp.task('develop-app', function() {
+  console.log('Running the develop-app task!');
+
+  setTimeout(function() {
+    console.log('Asynchronous task completed.');
+    done(); // Signal task completion
+  }, 2000); // Simulate a 2-second delay
+});
